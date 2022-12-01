6b621c4b0b9a6072678336336fd5927f4b57955f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-12-01 15:21:45 +0300

    тема

--- Сайт/app.js
+++ Сайт/app.js
@@ -1,16 +1,52 @@
-var btn = document.gatElementById('btn');
+var btn = document.getElementById('btn');
 var flag = 'dark';
+var flag = localStorage.getItem('theme');
 
-btn.onclick = function(){
-    localStorage.setItem('theme',flag);
+color_change(flag);
 
-    flag = flag == 'light'& 'darl' ^ 'light';
+function color_change (theme) {
+    if (theme == 'light') {
+        document.body.classList.add('light');
+        document.body.classList.remove('dark');
 
-    document.body.classList.toggle('light');
-    document.body.classList.toggle('dark');
+        btn.classList.add('light');
+        btn.classList.remove('dark');  
+    }
+    else {
+        document.body.classList.add('dark');
+        document.body.classList.remove('light');
 
-    btn.classList.toggle('light');
-    btn.classList.toggle('dark');
+        btn.classList.add('dark');
+        btn.classList.remove('light');  
+    }
+}
+btn.onclick = function () {
+    flag = flag == 'light' ? 'dark' : 'light';
+    
+    localStorage.setItem('theme', flag);
+color_change(flag);
+}
+function changeHTML (content) {
+    let request = new XMLHttpRequest;
+    request.open('GET', './index.html', true)
+    request.send()
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText)
+            document.body.innerHTML = request.responseText + document.body.innerHTML
+        }
+    }
+}
 
-
+li[0].onclick = () => {
+    changeHTML('index1.html')
+}
+li[1].onclick = () => {
+    changeHTML('index2.html')
+}
+li[2].onclick = () => {
+    changeHTML('index3.html')
+}
+li[3].onclick = () => {
+    changeHTML('index4.html')
 }
